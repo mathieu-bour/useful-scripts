7f7b7bf32674d1853b93c2bc1ddb77fbdf748b32
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-02-28 00:25:10 +0100

    fix(portainer): restart always

--- portainer/portainer-start.sh
+++ portainer/portainer-start.sh
@@ -4,6 +4,7 @@ IMAGE="portainer/portainer:${VERSION}"
 
 docker run -d \
   --name portainer \
+  --restart always \
   -p 8000:8000 \
   -p 9090:9000 \
   -v /var/run/docker.sock:/var/run/docker.sock \

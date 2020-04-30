efc9d417af8c59fd14c2f820d40e17459e5b8449
Mathieu Bour <mathieu.tin.bour@gmail.com>
2020-04-30 07:34:02 +0200

    feat(k8s): add kubectl and kustomize

---
+++ kustomize/install-ubuntu.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+chmod +x ./kustomize
+sudo mv ./kustomize /usr/local/bin/kustomize
